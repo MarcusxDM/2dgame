390a8349c2c84c12672e9d1039ed79989b242bb2
Marcus Vinicius <marcuspestana@ic.ufal.br>
2020-03-10 18:19:45 -0300

    jump & run animations
    need jump sheet fix

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -5,9 +5,8 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public  CharacterController2D controller;
-    
+    public Animator animator;
     public float horizontalMove = 40f;
-
     float runSpeed = 30f;
 
     bool jump = false;
@@ -18,11 +17,20 @@ public class PlayerMovement : MonoBehaviour
     {
         horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
 
+        animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
+
         if(Input.GetButtonDown("Jump"))
         {
             jump = true;
             runSpeed = runSpeed * 2;
+            animator.SetBool("IsJumping", true);
         }
+        
+    }
+
+    public void OnLanding()
+    {
+        animator.SetBool("IsJumping", false);
     }
 
     void FixedUpdate()
